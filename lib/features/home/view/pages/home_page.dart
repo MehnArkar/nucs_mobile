abe57c7e257ff2888487bfc29a14f561321713bc
Arkar <arkarmin.dev@gmail.com>
2025-03-14 20:13:32 +0700

    feat(UI) implement home page image slider

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nucs_mobile/config/constants/app_dimension.dart';
+import 'package:nucs_mobile/features/home/view/widgets/feature_images_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +8,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container()
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical:AppDimension.paddingPage),
+        children: [
+          FeatureImagesSlider()
+        ],
+      )
     );
   }
 }
